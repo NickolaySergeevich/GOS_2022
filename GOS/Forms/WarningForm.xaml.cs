fd0e255084c62895f35f3e3aef26cbf8ac9aeeb8
Николай Алексеев <asakura475@gmail.com>
2022-06-08 19:54:06 +0300

    Дописываю авторизацию

---
+++ GOS/Forms/WarningForm.xaml.cs
@@ -0,0 +1,29 @@
+﻿using System.Windows;
+
+namespace GOS.Forms
+{
+    /// <summary>
+    /// Форма с предупреждением.
+    /// Имеет публичный Label label_warningText - в него надо вносить сообщение.
+    /// </summary>
+    public partial class WarningForm
+    {
+        /// <summary>
+        /// Обычный конструктор
+        /// </summary>
+        public WarningForm()
+        {
+            InitializeComponent();
+        }
+
+        /// <summary>
+        /// Закрытие формы по кнопке
+        /// </summary>
+        /// <param name="sender">Кто отправил сообщение</param>
+        /// <param name="e">Аргументы клика</param>
+        private void Button_exit_OnClick(object sender, RoutedEventArgs e)
+        {
+            Close();
+        }
+    }
+}
--- GOS/forms/AuthorizationForm.xaml.cs
+++ GOS/forms/AuthorizationForm.xaml.cs
@@ -1,10 +1,53 @@
-﻿namespace GOS.forms
+﻿using System.Diagnostics;
+using System.Windows;
+
+using GOS.Forms;
+using GOS.WorkWithDB;
+
+namespace GOS.forms
 {
+    /// <summary>
+    /// Форма авторизации
+    /// </summary>
     public partial class AuthorizationForm
     {
         public AuthorizationForm()
         {
             InitializeComponent();
+        }
+
+        /// <summary>
+        /// Обработка входа в приложение
+        /// </summary>
+        /// <param name="sender">Кто отправил событие</param>
+        /// <param name="e">Аргументы</param>
+        private void Button_login_OnClick(object sender, RoutedEventArgs e)
+        {
+            if (string.IsNullOrEmpty(textBox_email.Text) || string.IsNullOrEmpty(passwordBox_password.Password))
+            {
+                var warningForm = new WarningForm
+                {
+                    label_warningText =
+                    {
+                        Content = "Не заполнены поля для входа!"
+                    }
+                };
+                warningForm.ShowDialog();
+
+                return;
+            }
+
+            Debug.WriteLine("\tMESSAGE {0}", WorkWithDb.Instance.Login(textBox_email.Text, passwordBox_password.Password)["RoleID"]);
+        }
+
+        /// <summary>
+        /// Выход из приложения
+        /// </summary>
+        /// <param name="sender">Кто отправил событие</param>
+        /// <param name="e">Аргументы</param>
+        private void Button_exit_OnClick(object sender, RoutedEventArgs e)
+        {
+            Close();
         }
     }
 }
