c0413baaa8814837ca72539c870467e22a21bd11
Николай Алексеев <asakura475@gmail.com>
2022-06-08 19:17:53 +0300

    Настроил работу с бд по синглтону

---
+++ GOS/WorkWithDB/WorkWithDB.cs
@@ -0,0 +1,73 @@
+﻿using System.Collections.Generic;
+using System.IO;
+
+using MySql.Data.MySqlClient;
+
+namespace GOS.WorkWithDB
+{
+    /// <summary>
+    /// Класс работы с базой данных.
+    /// Использует паттерн singleton.
+    /// </summary>
+    public sealed class WorkWithDb
+    {
+        /// <summary>
+        /// Файл настроек - указывать в первую очередь!
+        /// </summary>
+        private const string SettingsFile = "../../WorkWithDB/HelpFiles/DatabaseSettings.dk";
+
+
+        /// <summary>
+        /// Для реализации паттерна singleton
+        /// </summary>
+        private static WorkWithDb _instance;
+
+        /// <summary>
+        /// Для реализации паттерна singleton
+        /// </summary>
+        public static WorkWithDb Instance => _instance ?? (_instance = new WorkWithDb(ReadSettingsFile()));
+
+        /// <summary>
+        /// Подключение к бд
+        /// </summary>
+        private readonly MySqlConnection _connection;
+
+        /// <summary>
+        /// Приватный конструктор для паттерна singleton.
+        /// Для использования сначала укажите файл настроек.
+        /// </summary>
+        /// <param name="settingsDict">Словарь с настройками, берется из метода <c>ReadSettingsFile</c></param>
+        private WorkWithDb(IReadOnlyDictionary<string, string> settingsDict)
+        {
+            _connection = new MySqlConnection($"server={settingsDict["host"]};userid={settingsDict["username"]};password={settingsDict["password"]};database={settingsDict["databaseName"]}");
+            _connection.Open();
+        }
+
+        /// <summary>
+        /// Деструктор - закрывает соединение с бд
+        /// </summary>
+        ~WorkWithDb()
+        {
+            Instance._connection.Close();
+        }
+
+        /// <summary>
+        /// Чтение файла настроек.
+        /// Обязательно укажите настройки в нужном поле!
+        /// </summary>
+        /// <returns>Словарь вида параметр=значение</returns>
+        private static Dictionary<string, string> ReadSettingsFile()
+        {
+            var answer = new Dictionary<string, string>();
+
+            var text = File.ReadAllText(SettingsFile);
+            foreach (var settingsLine in text.Split('\n'))
+            {
+                var settingsLineSplit = settingsLine.Split('=');
+                answer[settingsLineSplit[0]] = settingsLineSplit[1];
+            }
+
+            return answer;
+        }
+    }
+}
--- GOS/forms/AuthorizationForm.xaml.cs
+++ GOS/forms/AuthorizationForm.xaml.cs
@@ -1,27 +1,15 @@
-﻿using System;
-using System.Collections.Generic;
-using System.Linq;
-using System.Text;
-using System.Threading.Tasks;
-using System.Windows;
-using System.Windows.Controls;
-using System.Windows.Data;
-using System.Windows.Documents;
-using System.Windows.Input;
-using System.Windows.Media;
-using System.Windows.Media.Imaging;
-using System.Windows.Shapes;
+﻿
+using GOS.WorkWithDB;
 
 namespace GOS.forms
 {
-    /// <summary>
-    /// Логика взаимодействия для AuthorizationForm.xaml
-    /// </summary>
-    public partial class AuthorizationForm : Window
+    public partial class AuthorizationForm
     {
         public AuthorizationForm()
         {
             InitializeComponent();
+
+            var test = WorkWithDb.Instance;
         }
     }
 }
