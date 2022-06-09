759aa8de01644f2801d5ea583bfee7806ae29ba8
Николай Алексеев <asakura475@gmail.com>
2022-06-09 20:34:17 +0300

    Начал работу над формой изменения пользователя

--- GOS/forms/AdministratorForm.xaml.cs
+++ GOS/forms/AdministratorForm.xaml.cs
@@ -7,41 +7,6 @@ using GOS.WorkWithDB;
 namespace GOS.Forms
 {
     /// <summary>
-    /// Класс для отображения данных о пользователе
-    /// </summary>
-    public class UserItem
-    {
-        /// <summary>
-        /// Имя пользователя
-        /// </summary>
-        public string Name { get; set; }
-        /// <summary>
-        /// Фамилия пользователя
-        /// </summary>
-        public string LastName { get; set; }
-        /// <summary>
-        /// Возраст пользователя
-        /// </summary>
-        public string Age { get; set; }
-        /// <summary>
-        /// Роль пользователя в системе
-        /// </summary>
-        public string UserRole { get; set; }
-        /// <summary>
-        /// Почтовый адрес пользователя
-        /// </summary>
-        public string EmailAddress { get; set; }
-        /// <summary>
-        /// Офис, к которому прикреплен пользователь
-        /// </summary>
-        public string Office { get; set; }
-        /// <summary>
-        /// Задний фон пользователя
-        /// </summary>
-        public string BackColor { get; set; }
-    }
-
-    /// <summary>
     /// Класс окна администратора
     /// </summary>
     public partial class AdministratorForm
--- GOS/forms/EditRoleForm.xaml.cs
+++ GOS/forms/EditRoleForm.xaml.cs
@@ -1,10 +1,47 @@
-﻿namespace GOS.Forms
+﻿using System.Windows;
+
+using GOS.WorkWithDB;
+
+namespace GOS.Forms
 {
     public partial class EditRoleForm
     {
-        public EditRoleForm()
+        /// <summary>
+        /// Конструктор
+        /// <br>Заполняет <c>ComboBox</c> с офисами</br>
+        /// <br>Заполняет данные о пользователе</br>
+        /// </summary>
+        public EditRoleForm(UserItem user)
         {
             InitializeComponent();
+
+            foreach (var officeName in WorkWithDb.Instance.GetAllOfficesForAdmin())
+                comboBox_offices.Items.Add(officeName);
+            comboBox_offices.SelectedIndex = 0;
+
+            textBox_emailAddress.Text = user.EmailAddress;
+            textBox_firstName.Text = user.Name;
+            textBox_lastName.Text = user.LastName;
+            comboBox_offices.SelectedIndex = comboBox_offices.Items.IndexOf(user.Office);
+            switch (user.UserRole)
+            {
+                case "User":
+                    radioButton_user.IsChecked = true;
+                    break;
+                case "Administrator":
+                    radioButton_administrator.IsChecked = true;
+                    break;
+            }
+        }
+
+        private void Button_apply_OnClick(object sender, RoutedEventArgs e)
+        {
+
+        }
+
+        private void Button_cancel_OnClick(object sender, RoutedEventArgs e)
+        {
+            Close();
         }
     }
 }
