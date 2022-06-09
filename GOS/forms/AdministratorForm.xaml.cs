599865ee5bb4cb7a4c6fccd8e847aa492a52d9f4
Николай Алексеев <asakura475@gmail.com>
2022-06-09 17:50:51 +0300

    Настроил отображение пользователя

--- GOS/forms/AdministratorForm.xaml.cs
+++ GOS/forms/AdministratorForm.xaml.cs
@@ -1,27 +1,74 @@
-﻿using System;
-using System.Collections.Generic;
-using System.Linq;
-using System.Text;
-using System.Threading.Tasks;
-using System.Windows;
-using System.Windows.Controls;
-using System.Windows.Data;
-using System.Windows.Documents;
-using System.Windows.Input;
-using System.Windows.Media;
-using System.Windows.Media.Imaging;
-using System.Windows.Shapes;
+﻿using System.Collections.Generic;
+
+using GOS.WorkWithDB;
 
 namespace GOS.Forms
 {
     /// <summary>
-    /// Логика взаимодействия для AdministratorForm.xaml
+    /// Класс для отображения данных о пользователе
     /// </summary>
-    public partial class AdministratorForm : Window
+    public class UserItem
     {
+        /// <summary>
+        /// Имя пользователя
+        /// </summary>
+        public string Name { get; set; }
+        /// <summary>
+        /// Фамилия пользователя
+        /// </summary>
+        public string LastName { get; set; }
+        /// <summary>
+        /// Возраст пользователя
+        /// </summary>
+        public string Age { get; set; }
+        /// <summary>
+        /// Роль пользователя в системе
+        /// </summary>
+        public string UserRole { get; set; }
+        /// <summary>
+        /// Почтовый адрес пользователя
+        /// </summary>
+        public string EmailAddress { get; set; }
+        /// <summary>
+        /// Офис, к которому прикреплен пользователь
+        /// </summary>
+        public string Office { get; set; }
+        /// <summary>
+        /// Задний фон пользователя
+        /// </summary>
+        public string BackColor { get; set; }
+    }
+
+    /// <summary>
+    /// Класс окна администратора
+    /// </summary>
+    public partial class AdministratorForm
+    {
+        /// <summary>
+        /// Конструктор - здесь создается список пользователей и прикрепляется к <c>ListBox</c>
+        /// </summary>
         public AdministratorForm()
         {
             InitializeComponent();
+
+            var userItems = new List<UserItem>();
+            foreach (var user in WorkWithDb.Instance.GetAllUsersForAdmin())
+            {
+                var userItem = new UserItem
+                {
+                    Name = user["Name"],
+                    LastName = user["LastName"],
+                    Age = user["Age"],
+                    UserRole = user["UserRole"],
+                    EmailAddress = user["EmailAddress"],
+                    Office = user["Office"],
+                    BackColor = user["Active"] == "False" ? "Red" : "White"
+                };
+
+                userItems.Add(userItem);
+            }
+
+            listBox_users.ItemsSource = userItems;
         }
     }
 }
