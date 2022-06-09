3547799441cd5bae9ee4a3006e777c779f8ef770
Николай Алексеев <asakura475@gmail.com>
2022-06-09 23:43:18 +0300

    Добавил форму пользователя

---
+++ GOS/forms/UserForm.xaml.cs
@@ -0,0 +1,30 @@
+﻿using System.Windows;
+
+namespace GOS.Forms
+{
+    /// <summary>
+    /// Класс окна пользователя
+    /// </summary>
+    public partial class UserForm
+    {
+        /// <summary>
+        /// Конструктор
+        /// </summary>
+        public UserForm()
+        {
+            InitializeComponent();
+        }
+
+        /// <summary>
+        /// Обработка закрытия формы пользователя
+        /// </summary>
+        /// <param name="sender">Кто послал сигнал</param>
+        /// <param name="e">Аргументы</param>
+        private void MenuItem_exit_OnClick(object sender, RoutedEventArgs e)
+        {
+            var authorizationForm = new AuthorizationForm();
+            authorizationForm.Show();
+            Close();
+        }
+    }
+}
