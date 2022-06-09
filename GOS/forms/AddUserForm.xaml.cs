a2a689e9507dce159917319c52cfe890187118ce
Николай Алексеев <asakura475@gmail.com>
2022-06-09 19:43:41 +0300

    Начал писать добавление пользователя в бд через форму

--- GOS/forms/AddUserForm.xaml.cs
+++ GOS/forms/AddUserForm.xaml.cs
@@ -16,6 +16,7 @@ namespace GOS.Forms
 
             foreach (var officeName in WorkWithDb.Instance.GetAllOfficesForAdmin())
                 comboBox_offices.Items.Add(officeName);
+            comboBox_offices.SelectedIndex = 0;
         }
 
         /// <summary>
@@ -25,7 +26,24 @@ namespace GOS.Forms
         /// <param name="e">Аргументы</param>
         private void Button_save_OnClick(object sender, RoutedEventArgs e)
         {
+            if (string.IsNullOrEmpty(textBox_email.Text) || string.IsNullOrEmpty(textBox_firstName.Text) ||
+                string.IsNullOrEmpty(textBox_lastName.Text) || string.IsNullOrEmpty(passwordBox_password.Password) ||
+                string.IsNullOrEmpty(datePicker_birthdate.Text))
+            {
+                HelpMethods.ShowWarning("Не все поля заполнены!");
+                return;
+            }
 
+            WorkWithDb.Instance.InsertUserIntoDb(textBox_email.Text, textBox_firstName.Text, textBox_lastName.Text,
+                comboBox_offices.SelectedItem.ToString(), datePicker_birthdate.Text, passwordBox_password.Password);
+
+            textBox_email.Text = string.Empty;
+            textBox_firstName.Text = string.Empty;
+            textBox_lastName.Text = string.Empty;
+            datePicker_birthdate.Text = string.Empty;
+            passwordBox_password.Password = string.Empty;
+
+            HelpMethods.ShowMessage("Пользователь успешно добавлен в бд!");
         }
 
         /// <summary>
