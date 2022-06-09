bbfa5ed6e0bf1f66f4d9083d3182b2e689ca248b
Николай Алексеев <asakura475@gmail.com>
2022-06-09 19:22:23 +0300

    начал писать логику окна

--- GOS/forms/AddUserForm.xaml.cs
+++ GOS/forms/AddUserForm.xaml.cs
@@ -1,27 +1,41 @@
-﻿using System;
-using System.Collections.Generic;
-using System.Linq;
-using System.Text;
-using System.Threading.Tasks;
-using System.Windows;
-using System.Windows.Controls;
-using System.Windows.Data;
-using System.Windows.Documents;
-using System.Windows.Input;
-using System.Windows.Media;
-using System.Windows.Media.Imaging;
-using System.Windows.Shapes;
+﻿using System.Windows;
+
+using GOS.WorkWithDB;
 
 namespace GOS.Forms
 {
-    /// <summary>
-    /// Логика взаимодействия для AddUserForm.xaml
-    /// </summary>
-    public partial class AddUserForm : Window
+    public partial class AddUserForm
     {
+        /// <summary>
+        /// Конструктор
+        /// <br>Заполняет <c>ComboBox</c> с офисами</br>
+        /// </summary>
         public AddUserForm()
         {
             InitializeComponent();
+
+            foreach (var officeName in WorkWithDb.Instance.GetAllOfficesForAdmin())
+                comboBox_offices.Items.Add(officeName);
+        }
+
+        /// <summary>
+        /// Сохраняет пользователя в бд и очищает поля для ввода
+        /// </summary>
+        /// <param name="sender">Кто отправил сигнал</param>
+        /// <param name="e">Аргументы</param>
+        private void Button_save_OnClick(object sender, RoutedEventArgs e)
+        {
+
+        }
+
+        /// <summary>
+        /// Закрытие формы добавления пользователя
+        /// </summary>
+        /// <param name="sender">Кто отправил сигнал</param>
+        /// <param name="e">Аргументы</param>
+        private void Button_cancel_OnClick(object sender, RoutedEventArgs e)
+        {
+            Close();
         }
     }
 }
